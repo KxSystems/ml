dac6711db9f6433fcf78940b321bd7f563969d54
Fionnuala Carr <fcarr@kx.com>
2018-10-16 17:21:47 +0100

    removed paths

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -1,4 +1,3 @@
-set OP=%PATH%
 if "%APPVEYOR_REPO_TAG%"=="true" (
  set ML_VERSION=%APPVEYOR_REPO_TAG_NAME%
 ) else (
@@ -11,7 +10,6 @@ perl -p -i.bak -e s/TOOLKITVERSION/`\$\"%ML_VERSION%\"/g ml.q
 if not defined QLIC_KC (
  goto :nokdb
 )
-set PATH=%OP%
 call "build\getkdb.bat" || goto :error
 
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
