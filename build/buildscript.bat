2e45b41dbd278fe64b924f55953fbe5173c1ad28
diane <dodonoghue@kx.com>
2019-02-26 15:45:07 +0000

    test

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -14,7 +14,7 @@ call "build\getkdb.bat" || goto :error
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
 mkdir embedpy
 cd embedpy
-echo "check"
+echo "here"
 echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
 cd ..
 echo p)print('embedpy runs') | q -q || goto :error
