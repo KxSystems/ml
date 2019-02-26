962fab134b42e30357e53cef85a422d208f24eed
diane <dodonoghue@kx.com>
2019-02-26 17:41:36 +0000

    added conda cmd

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -9,21 +9,27 @@ perl -p -i.bak -e s/NLPVERSION/`\$\"%NLP_VERSION%\"/g nlp.q
 if not defined QLIC_KC (
  goto :nokdb
 )
-call "build\getkdb.bat" || goto :error
 
+@echo OFF
+set PATH=%OP%
+call build\getkdb.bat                                || goto :error
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
 conda init cmd.exe
 mkdir embedpy
 cd embedpy
 echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
 cd ..
-echo p)print('embedpy runs') | q -q || goto :error
+pip install -r requirements.txt                      || goto :error
+call install.bat                                     || goto :error
 exit /b 0
 
 :error
-echo failed with error 
-exit /b 
+echo failed with error %errorLevel%
+set PATH=%OP%
+exit /b %errorLevel%
 
 :nokdb
 echo no kdb
+set PATH=%OP%
 exit /b 0
+
