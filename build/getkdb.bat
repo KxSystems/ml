63f9ce82f13007e572a12d6815ddb3508ced3677
diane <dodonoghue@kx.com>
2019-02-26 16:24:34 +0000

    test

--- build/getkdb.bat
+++ build/getkdb.bat
@@ -3,6 +3,9 @@ mkdir q
 7z x w64.zip -oq
 echo|set /P =%QLIC_KC% >q\kc.lic.enc
 certutil -decode q\kc.lic.enc q\kc.lic
+SET PATH=%PYTHON%;%PYTHON%\\Scripts;%PATH%"
+SET PYTHONPATH=%PYTHON%\\Lib\\site-packages;%PYTHONPATH%"
+
 set QHOME=%cd%\q
 set PATH=%QHOME%\w64;%PATH%
 echo .z.K | q -q
