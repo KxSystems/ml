64358b5b4b94589754b2999d74894e9f898fd782
diane <dodonoghue@kx.com>
2019-03-07 10:53:20 +0000

    test

--- build/test.bat
+++ build/test.bat
@@ -2,5 +2,5 @@ if defined QLIC_KC (
         pip -q install -r requirements.txt
 	echo getting test.q from embedpy
         curl -fsSL -o test.q https://github.com/KxSystems/embedpy/raw/master/test.q
-        q test.q util/tests fresh/tests tests xval/tests -q 
+        q test.q util/tests fresh/tests tests/ xval/tests -q 
 )
