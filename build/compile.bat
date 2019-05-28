93429818201d9a84e046007c2286dace63b7f585
diane <dodonoghue@kx.com>
2019-05-28 16:59:53 +0100

    complib

--- build/compile.bat
+++ build/compile.bat
@@ -4,5 +4,5 @@ mkdir src\lib
 mkdir src\include
 curl -fsSL -o src\include\k.h https://github.com/KxSystems/kdb/raw/master/c/c/k.h
 curl -fsSL -o src\lib\q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib
-cl /LD /DKXVER=3 /Feml.dll q.lib
+cl /LD /DKXVER=3 /Feml.dll src\lib\q.lib
 
