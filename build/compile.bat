c5f7a019c5e94012c1f093c895a8a545591ab299
diane <dodonoghue@kx.com>
2019-05-28 16:58:21 +0100

    comp

--- build/compile.bat
+++ build/compile.bat
@@ -4,5 +4,5 @@ mkdir src\lib
 mkdir src\include
 curl -fsSL -o src\include\k.h https://github.com/KxSystems/kdb/raw/master/c/c/k.h
 curl -fsSL -o src\lib\q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib
-cl /LD /Few64\jupyterq.dll src\lib\q.lib /DKXVER=3 /Isrc\include src\c\jupyterq.c
+cl /LD /DKXVER=3 /Feml.dll q.lib
 
