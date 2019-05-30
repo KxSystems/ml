bb7ab0c2755cc472b83308e4a595d3775a8217af
diane <dodonoghue@kx.com>
2019-05-30 16:51:46 +0100

    moved to clust/ccode

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -4,10 +4,10 @@ curl -fsSL -o q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib || got
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-mkdir w64
-cl /LD /Few64\cure.dll /Few64\kdtree.dll /DKXVER=3 clust/ccode/cure.c clust/ccode/kdtree.c  q.lib                                  || goto :error
 
-move cure.dll kdtree.dll w64
+cl /LD /DKXVER=3 clust/ccode/cure.c clust/ccode/kdtree.c  q.lib                                  || goto :error
+
+move cure.dll kdtree.dll clust/ccode
 
 
 set PATH=%OP%
