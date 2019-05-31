d09d388b4426d4dcb86e7c6704d55ba71bf3f49d
diane <dodonoghue@kx.com>
2019-05-31 16:38:45 +0100

    added dll to package.bat

--- build/package.bat
+++ build/package.bat
@@ -1,3 +1,3 @@
-7z a ml_windows-%ML_VERSION%.zip *.q fresh util requirements.txt  LICENSE README.md
+7z a ml_windows-%ML_VERSION%.zip *.q fresh util clust requirements.txt clust/ccode/cure.dll clust/ccode/kdtree.dll LICENSE README.md
 appveyor PushArtifact ml_windows-%ML_VERSION%.zip
 exit /b 0
