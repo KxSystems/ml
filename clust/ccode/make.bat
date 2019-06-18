913f0ab4eed1aafa942c5bc7e6d0596be0f4898e
diane <dodonoghue@kx.com>
2019-06-18 15:50:01 +0100

    add call

--- clust/ccode/make.bat
+++ clust/ccode/make.bat
@@ -1,5 +1,5 @@
 set PATH=%OP%
-ren 'C:\Program Files (x86)\Windows Kits\10\include\wdf' '00wdf'
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 cl /LD /DKXVER=3 kdtree.def kdtree.c q.lib
 cl /LD /DKXVER=3 cure.def cure.c q.lib kdtree.lib
