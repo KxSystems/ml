1cbe9c1124718c13fc82e6ce2297dec6d17fd4be
diane <dodonoghue@kx.com>
2019-06-18 15:12:53 +0100

    previous vs

--- clust/ccode/make.bat
+++ clust/ccode/make.bat
@@ -1,5 +1,4 @@
 set PATH=%OP%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-ren 'C:\Program Files (x86)\Windows Kits\10\include\wdf' '00wdf'
 cl /LD /DKXVER=3 kdtree.def kdtree.c q.lib
 cl /LD /DKXVER=3 cure.def cure.c q.lib kdtree.lib
