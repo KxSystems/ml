1e4898ef29806297bed351dfa3e31d21aa33997f
diane <dodonoghue@kx.com>
2019-06-19 10:19:24 +0100

    2015 vs

--- clust/ccode/make.bat
+++ clust/ccode/make.bat
@@ -1,4 +1,5 @@
 set PATH=%OP%
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
 cl /LD /DKXVER=3 kdtree.def kdtree.c q.lib
 cl /LD /DKXVER=3 cure.def cure.c q.lib kdtree.lib
