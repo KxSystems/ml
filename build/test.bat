a4412c822d06607187c6d4f6afc7111dd7bc426e
diane <dodonoghue@kx.com>
2019-10-11 16:12:02 +0100

    fixed travis and appveyor

--- build/test.bat
+++ build/test.bat
@@ -1,5 +1,4 @@
 if defined QLIC_KC (
-        apt-get install libhunspell-dev
         pip -q install -r requirements.txt
         python -m spacy download en
         echo getting test.q from embedpy
