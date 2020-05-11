89e3040c9ab2cbcc3fbd2f307b586beb3735dd4e
cmccarthy1 <38653604+cmccarthy1@users.noreply.github.com>
2020-05-11 22:27:03 +0100

    Update package.bat

--- build/package.bat
+++ build/package.bat
@@ -1,3 +1,3 @@
-7z a automl_windows-%AUTOML_VERSION%.zip *.q requirements.txt code/ LICENSE README.md
+7z a automl_windows-%AUTOML_VERSION%.zip *.q requirements.txt code LICENSE README.md
 appveyor PushArtifact automl_windows-%AUTOML_VERSION%.zip
 exit /b 0
